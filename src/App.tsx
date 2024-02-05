4d415ae361e8d0fea0ad583ba0980cf1b6da2b85
Oleg Muhachev <olmu1996@gmail.com>
2024-02-05 14:50:25 +0300

    feat: clear files and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import files from "./context";
 import "./App.css";
 
 function App() {
-  const filesList = Object.keys(files).map(fileName => ({
-    name: fileName,
-    url: files[fileName],
-  }))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {/* <a
-          className="App-link"
-          // href={file}
-          target="_blank"
-          rel="noreferrer"
-          download="Example"
-        >
-          View docs
-        </a> */}
-        {filesList.map((file, index) => (
-          <li key={index}>
-            <a href={file.url} download={file.name}>
-              {file.name.slice(2)}
-            </a>
-          </li>
-        ))}
-      </header>
     </div>
   );
 }
